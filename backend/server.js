3321139adb9c6236ad23464b2b5eb14ef1c2f365
amos-kibet <kibetamos97@gmail.com>
2022-05-22 17:46:52 +0300

    Fix typo
    Change server connection logging message from 'server runnign development mode on port 5000' to 'Server running in development mode on port 5000.'

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,55 @@
-import path from 'path';
-import express  from 'express'
-import dotenv  from 'dotenv'
-import connectDB from './config/db.js'
-import morgan from 'morgan'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import  productRoutes  from './routes/productRoutes.js';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import morgan from "morgan";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-if(process.env.NODE_ENV === 'developement'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "developement") {
+  app.use(morgan("dev"));
 }
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',orderRoutes)
-app.get('/api/config/paypal',(req,res)=> res.send(process.env.PAYPAL_CLIENT_ID))
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-const __dirname= path.resolve()
+const __dirname = path.resolve();
 
-
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
-}else{
-    app.get('/', (req,res) =>{
-        res.send('API is Runn....')
-    })
-    
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Runn....");
+  });
 }
 
+app.use(notFound);
+app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
 
-app.use(notFound)
-app.use(errorHandler)
-
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server runingin ${process.env.NODE_ENV } mode on port ${PORT}`.yellow.bold))
+//runingin
